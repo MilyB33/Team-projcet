427479819a1fb85a498bb409c19e325f8c549c6e
Miłosz Bolewski <milosz.bolewski.33@gmail.com>
2024-11-26 19:00:24 +0100

    Add login backend

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+// initialize the Prisma Client
+const prisma = new PrismaClient();
+
+const roundsOfHashing = 10;
+
+async function main() {
+  // create two dummy users
+  const passwordSabin = await bcrypt.hash('password-sabin', roundsOfHashing);
+  const passwordAlex = await bcrypt.hash('password-alex', roundsOfHashing);
+
+  const user1 = await prisma.user.upsert({
+    where: { email: 'sabin@adams.com' },
+    update: {
+      password: passwordSabin,
+    },
+    create: {
+      email: 'sabin@adams.com',
+      name: 'Sabin Adams',
+      password: passwordSabin,
+    },
+  });
+
+  const user2 = await prisma.user.upsert({
+    where: { email: 'alex@ruheni.com' },
+    update: {
+      password: passwordAlex,
+    },
+    create: {
+      email: 'alex@ruheni.com',
+      name: 'Alex Ruheni',
+      password: passwordAlex,
+    },
+  });
+
+  console.log({ user1, user2 });
+}
+
+// execute the main function
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    // close the Prisma Client at the end
+    await prisma.$disconnect();
+  });
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), PrismaModule, AuthModule],
   controllers: [],
   providers: [],
 })
