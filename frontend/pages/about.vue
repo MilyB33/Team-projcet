6070889646176eec43de1ef2a70f8d14fb782a55
mateu <a@>
2025-03-12 17:24:32 +0100

    details form

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -36,7 +36,6 @@
 </template>
 
 <script>
-import { ref } from "vue";
 
 export default {
   name: "AboutClocker",
--- frontend/pages/account/index.vue
+++ frontend/pages/account/index.vue
@@ -3,14 +3,14 @@
     <v-card class="pa-5" outlined>
       <v-card-title>Personal Details</v-card-title>
       <v-card-text>
-        <v-form>
+        <v-form @submit="onSubmit">
           <v-text-field label="Account Type" v-model="accountType" variant="outlined" disabled></v-text-field>
           <div class="name d-flex ga-4 mt-2 mb-2">
-            <v-text-field label="First Name" v-model="firstName" variant="outlined" :error-messages="firstNameError" :rules="[maxLengthRule]" counter="64" maxlength="64"></v-text-field>
-            <v-text-field label="Last Name" v-model="lastName" variant="outlined" :error-messages="lastNameError" :rules="[maxLengthRule]" counter="64" maxlength="64"></v-text-field>
+            <v-text-field label="First Name" v-model="firstName" variant="outlined" :error-messages="firstNameError"></v-text-field>
+            <v-text-field label="Last Name" v-model="lastName" variant="outlined" :error-messages="lastNameError"></v-text-field>
           </div>
-          <v-text-field label="Email Address" v-model="email" variant="outlined" :error-messages="emailError" type="email" :rules="[maxLengthRule]" counter="64" maxlength="64"></v-text-field>
-          <v-btn color="success" class="mt-3" @click="saveChanges">Save Changes</v-btn>
+          <v-text-field label="Email Address" v-model="email" variant="outlined" :error-messages="emailError" type="email"></v-text-field>
+          <v-btn color="success" class="mt-3" type="submit">Save Changes</v-btn>
           <v-btn color="primary" class="mt-3 ml-3" @click="resetPassword">Reset Password</v-btn>
         </v-form>
       </v-card-text>
@@ -18,9 +18,11 @@
   </v-container>
 </template>
 
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import {type User} from "~/types"
 
+const {user} = useUser();
+const {requestPasswordReset} = useAuth();
 const form = useForm({ validationSchema: personalDetailsTypedSchema });
 const { value: firstName, errorMessage: firstNameError } = useField("firstName");
 const { value: lastName, errorMessage: lastNameError } = useField("lastName");
@@ -28,8 +30,12 @@ const { value: email, errorMessage: emailError } = useField("email");
 const accountType = ref("IT engineer");
 
 const resetPassword = () => {
-  alert('Password reset link sent to email!');
+  if(user.value){
+    requestPasswordReset(user.value?.email)
+  }
 };
+
+const onSubmit = form.handleSubmit((values)=>{console.log(values)})
 
 const saveChanges = () => {
   if (firstNameError.value || lastNameError.value || emailError.value || !firstName.value || !lastName.value || !email.value) {
@@ -38,7 +44,32 @@ const saveChanges = () => {
   }
   alert('Changes saved successfully!');
 };
+const setInitialValues = (user: User) => {
+  form.setValues({
+    firstName: user.first_name,
+    lastName: user.last_name,
+    email: user.email,
+  });
 
+  if (user.typeId === 2) {
+    form.setValues({
+      // @ts-ignore
+      company: user.company,
+    });
+  }
+};
+
+watch(user, (newUser) => {
+  if (newUser) {
+    setInitialValues(newUser);
+  }
+});
+
+onMounted(() => {
+  if (user.value) {
+    setInitialValues(user.value);
+  }
+});
 </script>
 
 <style scoped>
