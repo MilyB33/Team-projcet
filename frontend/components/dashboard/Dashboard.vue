6939b33ec1374ebfb882b0b519dd326890d8b355
Miłosz Bolewski <milosz.bolewski.33@gmail.com>
2025-03-26 22:36:46 +0100

    add employer dashboard

--- frontend/components/dashboard/Dashboard.vue
+++ frontend/components/dashboard/Dashboard.vue
@@ -1,8 +1,16 @@
 <template>
-  <div v-if="isEmployer"></div>
+  <div
+    v-if="isEmployer"
+    class="d-flex justify-center align-center flex-column ga-4 mt-8"
+  >
+    <h1>Welcome back {{ user?.first_name }}!</h1>
+    <NuxtLink to="/projects">
+      <v-btn>Go to projects</v-btn>
+    </NuxtLink>
+  </div>
   <EmployeeDashboard v-else />
 </template>
 
 <script lang="ts" setup>
-const { isEmployer } = useUser();
+const { isEmployer, user } = useUser();
 </script>
